533a59906c12b5d48680b5bc78a106cdcb9495a7
Johan Castillo <jcjohan2707@gmail.com>
2020-01-03 12:55:47 -0400

    Objetos

--- ejercicio1/main.js
+++ ejercicio1/main.js
@@ -1,7 +1,18 @@
 //Ejercicio1
-//Destructurar la lista de amigos y separarlos en variables
-const amigos = ['Luis', 'Johan', 'Pedro';]
+//Destructurar objetos, clocarlos en variables separadas, 
+//acceder a ellos e imprimir su valor mediante "Strings Templates".
 
-const [amigo] = amigos;
+//Objeto
+const persona = {
+    nombre: "Johan",
+    apellido: "Castillo",
+    edad: 18,
+    pais: "Venezuela",
+    profesion: "Full-Stack Developer"
+}
 
-console.log(amigo);
+//Destructurando el objeto y separandolo en variables distintas.
+const { nombre, apellido, edad, pais, profesion = 'estudiante' } = persona;
+
+//Accediendo a el valor de las variables.
+console.log(`${nombre} ${apellido} tiene ${edad} años de edad, es de ${pais} y es ${profesion}.`);
