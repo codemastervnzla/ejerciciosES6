00254d93e8320bf0024832114ec591069c7eaea7
Johan Castillo <jcjohan2707@gmail.com>
2020-01-03 13:53:36 -0400

    Clases estaticas

---
+++ 04-clases_estaticas/main.js
@@ -0,0 +1,15 @@
+//Ejercico 4
+
+//Clase.
+class Usuario{
+    constructor(){
+        this.nombre = 'Johan',
+        this.apellido = 'Castillo',
+        this.edad = 18,
+        this.correo = 'correo@coreeo.com'
+    }
+}
+
+const Johan = new Usuario;
+
+console.log(Johan);
