7e1ae465e156ec4fb675112bb612cee6fe737906
Johan Castillo <jcjohan2707@gmail.com>
2020-01-03 22:17:05 -0400

    Clases Dinamicas

--- 04-clases_dinamicas/main.js
+++ 04-clases_dinamicas/main.js
@@ -1,15 +1,21 @@
 //Ejercico 4
 
-//Clase estática.
+//Clase dinámica. Llamada "Usuario".
 class Usuario{
-    constructor(){
-        this.nombre = 'Johan',
-        this.apellido = 'Castillo',
-        this.edad = 18,
-        this.correo = 'correo@coreeo.com'
+    constructor(nombre, apellido, edad, correo){
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.edad = edad,
+        this.correo = correo
     }
 }
 
-const Johan = new Usuario;
+//Usuario 1.
+const Johan = new Usuario('Johan', 'Castillo', 18, 'correo@correo.com');
 
 console.log(Johan);
+
+//Usuario 2.
+const Juan = new Usuario('Juan', 'Gonzales', 22, 'correo@correo.com');
+
+console.log(Juan)
