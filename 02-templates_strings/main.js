5d39f21bdb94758a30c69b1f701022a957eedd77
Johan Castillo <jcjohan2707@gmail.com>
2020-01-03 21:28:06 -0400

    Templates Strings

---
+++ 02-templates_strings/main.js
@@ -0,0 +1,10 @@
+
+//Variables.
+const nombre = 'Maria';
+
+const edad = '14';
+
+const actividad = 'jugar';
+
+//Template String
+console.log(`${nombre} tiene ${edad} años de edad y le gusta ${actividad}.`)
